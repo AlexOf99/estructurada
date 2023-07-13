42456523523e248be6c32929544605b56a4bea8a
AlexOf99 <ordazfragosoalexrobinson12@gmail.com>
2023-07-13 09:54:10 -0600

    Lectura de numeros

--- main.cpp
+++ main.cpp
@@ -4,4 +4,10 @@
 main()
 {
 	pintf("Hola mundo desde git");
+	int ni, n2;
+	printf("Ingresa n1: ");
+	sacanf("%d",&n1);
+	printf("ingresa n2: ");
+	scanf("%d",&n2);
+	printf("n1 = %d n2 = %d" ,n1,n2);
 }
